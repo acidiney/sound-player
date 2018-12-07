ca99321ab00cb1c180406e8b6041eb6357459e7c
Acidiney Dias <acidineydias@gmail.com>
2018-12-07 14:58:53 +0100

    Refatorando ui

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,24 @@
 <template>
-  <div id="app">
-    <app-menu />
-    <router-view/>
-    <app-player />
+  <div id="root">
+
   </div>
 </template>
-<script>
-import Menu from '@/components/Menu'
-import Player from '@/components/Player'
 
+<script>
 export default {
-  name: 'app',
-  components: {
-    'app-menu': Menu,
-    'app-player': Player
+  name: 'app-root',
+  data: function () {
+    return {
+
+    }
   }
 }
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css?family=Roboto");
-
-#app {
-  font-family: 'Roboto', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height: 100%;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  #root {
+    padding: 0;
+    margin: 0;
+    background: linear-gradient(to top, #333, #222);
   }
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,5 @@
 <template>
-  <div class="home">
-    <div class="list">
-      <app-card v-for="(i, index) in 6" :key="index"/>
-    </div>
-    <div class="list">
-      <app-card v-for="(i, index) in 6" :key="index"/>
-    </div>
-    <div class="list">
-      <app-card v-for="(i, index) in 6" :key="index"/>
-    </div>
-    <div class="list">
-      <app-card v-for="(i, index) in 6" :key="index"/>
-    </div>
-  </div>
+  <h1>
+    OO
+  </h1>
 </template>
-<script>
-// @ is an alias to /src
-import Card from '@/components/Card'
-export default {
-  name: 'home',
-  components: {
-    'app-card': Card
-  }
-}
-</script>
-<style lang="scss" scoped>
-div.home {
-  padding: 12px;
-  div.list {
-    display: flex;
-    justify-content: space-between;
-  }
-}
-</style>
